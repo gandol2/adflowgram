74ed7a4817823c3a17fad21f2b3fc1b3c56c569c
gandol2 <sskim2333@gmail.com>
2019-10-01 19:24:05 +0900

    인증메일 보내기 기능 구현

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
@@ -9,5 +12,5 @@ const server = new GraphQLServer({ schema });
 server.express.use(logger("dev")); // express 서버에 morgan log 미들웨어를 추가
 
 server.start({ port: PORT }, () =>
-  console.log(`✅✅✅ Server running on port http://localhost:${PORT}`)
+  console.log(`✅ Server running on port http://localhost:${PORT}`)
 );
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,35 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "sskim2333@gmail.com",
+    to: address,
+    subject: "🔒[ADFLOW GRAM] 로그인 Secret🔒",
+    html: `안녕하세요. ADFLOW GRAM 입니다.<br/>로그인 Secret 은 [${secret}] 입니다.<br/>로그인 Secret을 로그인 창에 입력 해 주세요.`
+  };
+
+  return sendMail(email);
 };
